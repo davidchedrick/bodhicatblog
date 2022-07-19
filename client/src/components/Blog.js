7d4fef0f7413140f48d27df3851fbc6f61c4d691
David Chedrick <davidchedrick@gmail.com>
2022-07-19 10:15:08 -0500

    added back button on blog

--- client/src/components/Blog.js
+++ client/src/components/Blog.js
@@ -1,59 +1,64 @@
 import { useEffect, useState } from "react";
+import { Button } from "react-bootstrap";
 import { useParams } from "react-router-dom";
 import Loading from "./Loading";
 // import ReactMarkdown from "react-markdown";
 
 const initialState = {
-  article: null,
-  error: null,
-  status: "pending",
+    article: null,
+    error: null,
+    status: "pending",
 };
 
-
 function Blog() {
-
-
     const [{ blog, error, status }, setState] = useState(initialState);
     const { id } = useParams();
-  
+
     useEffect(() => {
-      setState(initialState);
-      fetch(`/api/posts/${id}`).then((r) => {
-        if (r.ok) {
-          r.json().then((blog) => {
-            console.log('blog: ', blog);
-            setState({ blog, error: null, status: "resolved" })
-            
-          }
-          );
-        } else {
-          r.json().then((message) =>
-            setState({ blog: null, error: message.error, status: "rejected" })
-          );
-        }
-      });
+        setState(initialState);
+        fetch(`/api/posts/${id}`).then(r => {
+            if (r.ok) {
+                r.json().then(blog => {
+                    console.log("blog: ", blog);
+                    setState({ blog, error: null, status: "resolved" });
+                });
+            } else {
+                r.json().then(message =>
+                    setState({
+                        blog: null,
+                        error: message.error,
+                        status: "rejected",
+                    })
+                );
+            }
+        });
     }, [id]);
 
     if (status === "pending") return <Loading />;
 
-return(
-
-   
-        <article>
-           
-            <h1>cat</h1>
-        <h1>{blog.title}</h1>
-        <small>
-          <p>
-            {blog.date} 
-          </p>
-          <p>
-            <em>Written by {blog.author}</em>
-          </p>
-        </small>
-        <article>{blog.content}</article>
-      </article>
-    )
+    return (
+        <>
+            <Button
+                variant="dark"
+                size="sm"
+                className="m-2 position-absolute top-0 end-0"
+                href={"/"}
+            >
+                X
+            </Button>
+            <article className="blog">
+                <h1 className="b-title">{blog.title}</h1>
+                <small>
+                    <p>{blog.date}</p>
+                    <p>
+                        <em>Written by {blog.author}</em>
+                    </p>
+                </small>
+                <hr className="b-line" />
+                <div>{blog.content}</div>
+            </article>
+        </>
+    );
 }
 
-export default Blog
+export default Blog;
