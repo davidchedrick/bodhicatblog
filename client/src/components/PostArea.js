1f93219af113e1d8abe1dc20b36547990b8b2984
David Chedrick <davidchedrick@gmail.com>
2022-08-01 21:33:31 -0500

    PostArea refactor

--- client/src/components/PostArea.js
+++ client/src/components/PostArea.js
@@ -1,15 +1,21 @@
 import Blogs from "./Blogs";
 import Header from "./Header";
+import Loading from "./Loading";
 
 function PostArea({ currentUser, posts, handleLogout }) {
+    console.log('posts: ', posts.length);
     return (
         <>
             <Header currentUser={currentUser} handleLogout={handleLogout} />
-            <div>
+            
+            {posts.length === 0 ? <Loading /> :
+             <div>
                 {posts.map(post => (
                     <Blogs key={post.id} post={post} />
                 ))}
             </div>
+            }
+           
         </>
     );
 }
