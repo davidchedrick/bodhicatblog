60c84eaaf481676d78d7108bfb9a2d4689d2fd7f
David Chedrick <davidchedrick@gmail.com>
2022-08-03 12:25:17 -0500

    blogs refactor

--- client/src/components/Blogs.js
+++ client/src/components/Blogs.js
@@ -1,39 +1,28 @@
 import { Button, Card } from "react-bootstrap";
-import Loading from "./Loading";
 
 function Blogs({ post }) {
     return (
-        <>
-            {post.id === null ? (
-                <Loading />
-            ) : (
-                <Card className="text-center m-3">
-                    <Card.Header className="">
-                        Written By:
-                        {/* <a
-                            href={`/profiles/${post.profile.id}`}
-                            className="m-1 link-color"
-                        >
-                            {post.author}
-                        </a> */}
-                    </Card.Header>
-                    <Card.Body>
-                        <Card.Title>{post.title}</Card.Title>
-                        <Card.Text>{post.short_content}</Card.Text>
+        
+        <Card className="text-center m-3">
+            <Card.Header className="">
+                Written By:
+                <a
+                    href={`/profiles/${post.profile.id}`}
+                    className="m-1 link-color"
+                >
+                    {post.author}
+                </a>
+            </Card.Header>
+            <Card.Body>
+                <Card.Title>{post.title}</Card.Title>
+                <Card.Text>{post.short_content}</Card.Text>
 
-                        <Button
-                            variant="outline-danger"
-                            href={`/posts/${post.id}`}
-                        >
-                            Read
-                        </Button>
-                    </Card.Body>
-                    <Card.Footer className="text-muted">
-                        {post.date}
-                    </Card.Footer>
-                </Card>
-            )}
-        </>
+                <Button variant="outline-danger" href={`/posts/${post.id}`}>
+                    Read
+                </Button>
+            </Card.Body>
+            <Card.Footer className="text-muted">{post.date}</Card.Footer>
+        </Card>
     );
 }
 
